bfc132a36b6e0aad3cca10c5059876aaccd7c32c
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-19 11:38:19 -0300

    feat: req08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,17 +1,36 @@
-# Requisito 7
 from tech_news.database import search_news
+from datetime import datetime
+
+# from winreg import QueryValue
 
 
+# Requisito 7
 def search_by_title(title):
     news_list = search_news(
+        # options -i é para busca case-insensitive
         {"title": {"$regex": f".*{title}.*", "$options": "-i"}}
     )
+    # print(news_list)
+    # retorna uma lista de tuplas
     return [(news["title"], news["url"]) for news in news_list]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        # Converte a data para o formato dd/mm/AAAA
+        # from ISO format = fromsioformat()
+        dt = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    # Busca as notícias no banco de dados
+    news_list = search_news(
+        {
+            "timestamp": dt
+        }
+    )
+    return [(news["title"], news["url"]) for news in news_list]
 
 
 # Requisito 9
